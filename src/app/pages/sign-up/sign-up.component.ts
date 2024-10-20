633368da89395a6d104c400dd857dc013034a2aa
taghreedrabee <taghreedrabia6@gmail.com>
2024-10-20 16:32:40 +0300

    contact and about creation

--- src/app/pages/sign-up/sign-up.component.ts
+++ src/app/pages/sign-up/sign-up.component.ts
@@ -1,41 +1,50 @@
 import { Component } from '@angular/core';
-import {
-  FormControl,
-  FormGroup,
-  ReactiveFormsModule,
-  Validators,
-} from '@angular/forms';
+import {  FormGroup, Validators, FormControl,ReactiveFormsModule, AbstractControl, ValidatorFn   } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { Password } from 'primeng/password';
 import { Router } from '@angular/router';
 
+
 @Component({
   selector: 'app-SignUp',
   standalone: true,
-  imports: [ReactiveFormsModule, CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './sign-up.component.html',
-  styleUrl: './sign-up.component.css',
+  styleUrl: './sign-up.component.css'
 })
 export class SignUpComponent {
-  constructor(private router: Router) {}
+  passwordMismatch: boolean = false;
+  constructor( private router: Router) {}
   signup = new FormGroup({
-    email: new FormControl('', [Validators.required, Validators.email]),
-    text: new FormControl('', [Validators.required]),
-    Password: new FormControl('', [
-      Validators.required,
-      Validators.minLength(6),
-    ]),
-  });
+    email: new FormControl("", [Validators.required, Validators.email]),
+    username: new FormControl("", [Validators.required]),
+    phone: new FormControl("", [Validators.required]),
+    password: new FormControl("", [Validators.required, Validators.minLength(6)]), 
+    confirmPassword: new FormControl("", [Validators.required, Validators.minLength(6)])
+  }, { validators: this.passwordMatchValidator() });
 
-  onsignup() {
-    if (this.signup.valid) {
-      this.loginNavigate();
-    } else {
-      this.signup.markAllAsTouched();
+  passwordMatchValidator(): ValidatorFn {
+    return (group: AbstractControl): { [key: string]: any } | null => {
+      const password = group.get('password')?.value;  
+      const confirmPassword = group.get('confirmPassword')?.value;
+  
+      return password && confirmPassword && password !== confirmPassword
+        ? { passwordMismatch: true }
+        : null;
+    };
+  }
+  
+
+  
+    onsignup() {
+      if (this.signup.valid) {
+        this.loginNavigate();
+      } else {
+        this.signup.markAllAsTouched(); 
+      }
     }
-  }
 
-  loginNavigate() {
-    this.router.navigate(['/login']);
-  }
+    loginNavigate(){
+      this.router.navigate(['/login']);
+    }
 }
